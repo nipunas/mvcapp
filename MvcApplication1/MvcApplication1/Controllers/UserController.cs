5a01dd02b4e2abf6c9136994638ace0418f958b5
nipunasilva <nipunasi@99x.lk>
2014-07-06 16:43:02 +0530

    Commiting intial project files

---
+++ MvcApplication1/MvcApplication1/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using MvcApplication1.Models;
+using Operations.UserOperations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetUserInformation(string username)
+        {
+            HttpContext a = this.HttpContext.ApplicationInstance.Context;
+
+            var Ip = HttpContext.Request.ServerVariables["REMOTE_ADDR"];
+            var Ip2 = Request.ServerVariables["REMOTE_ADDR"];
+
+            UserOperator userOperator =new UserOperator();
+            string ipAddress = userOperator.ValidateIpAddress();
+
+            User user = new User() 
+            { 
+                UserId=1, 
+                UserName="Nipuna",
+                IpAddress = ipAddress
+            };
+
+            return Json(user, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MvcApplication1/Operations/UserOperations/UserOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Operations.UserOperations
+{
+    public class UserOperator
+    {
+        public string ValidateIpAddress()
+        {
+            HttpContext context = HttpContext.Current;
+            string Ip = context.Request.ServerVariables["REMOTE_ADDR"];
+
+            if (Ip.Contains("255"))
+            {
+                return "255.255.255.255";
+            }
+            else
+            {
+                return Ip;
+            }
+        }
+    }
+}
